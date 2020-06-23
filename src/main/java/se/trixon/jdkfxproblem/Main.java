e81c9b8097337862f2a7a192f97ec34e76a77a7a
Patrik Karlström <patrik@trixon.se>
2020-06-23 17:10:13 +0200

    Add failing code

---
+++ src/main/java/se/trixon/jdkfxproblem/Main.java
@@ -0,0 +1,16 @@
+package se.trixon.jdkfxproblem;
+
+/**
+ *
+ * @author Patrik Karlström
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println(new javafx.util.Pair<>("Hello", "world"));
+    }
+
+}
